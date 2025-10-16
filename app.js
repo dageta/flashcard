bd25fe45ff1cae9b1236ab9a0516cbba8fac5eb6
dageta <davi.linhares.nascimento@escola.pr.gov.br>
2025-10-16 07:30:24 -0300

    .

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-function CriaCartao(categoria, pergunta, resposta) {}
+function CriaCartao(categoria, pergunta, resposta) {
+     let container = document.getElementById('container');
+  let cartao = document.createElement('article');
+  cartao.className = 'cartao';
+}
+
+cartao.innerHTML =
+<div class="cartao_conteudo">
+    <h3>Programação</h3>
+    <div class="cartao_conteudo_pergunta">
+        <p>O que é javaScript?</p>
+</div>
+<div class="cartao_conteudo_resposta">
+    <p>O javaScript é uma linguagem de programação></p>
+        </div>
+        </div>
